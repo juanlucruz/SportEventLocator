64183bf49fb7ea0c00b9e6303264d7ef06040ec6
juanlucruz <juanlucruz@gmail.com>
2018-04-26 16:34:31 +0200

    update

---
+++ app.py
@@ -0,0 +1,30 @@
+# Import the Twython class
+from twython import Twython
+import json
+import pandas as pd
+
+# Load credentials from json file
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+# Instantiate an object
+python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+
+# Create our query
+query = {
+    'q': 'madrid',
+    'result_type': 'popular',
+    'lang': 'es',
+    'count': '250',
+}
+
+dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
+for status in python_tweets.search(**query)['statuses']:
+    dict_['user'].append(status['user']['screen_name'])
+    dict_['date'].append(status['created_at'])
+    dict_['text'].append(status['text'])
+    dict_['favorite_count'].append(status['favorite_count'])
+
+df = pd.DataFrame(dict_)
+df.sort_values(by='favorite_count', inplace=True, ascending=False)
+print(df.values[:10])
--- wms_test_script.py
+++ wms_test_script.py
@@ -2,7 +2,7 @@ from owslib.wms import WebMapService
 from owslib import crs
 from PIL import Image, ImageEnhance, ImageFilter
 import cv2
-import numpy as nps
+import numpy as np
 
 
 c = crs.Crs('EPSG:3857')
@@ -12,14 +12,14 @@ print('Type:',wms.identification.type)
 print('Title:',wms.identification.title)
 print('Contents:',list(wms.contents))
 for el in wms.contents:
-	print('*'*80)
-	print('\tTitle({}):{}'.format(el,wms[el].title))
-	print('\tQueryable:       ',wms[el].queryable)
-	print('\tOpaque:          ',wms[el].opaque)
-	print('\tBoundingBox:     ',wms[el].boundingBox)
-	print('\tBoundingBoxWGS84:',wms[el].boundingBoxWGS84)
-	print('\tcrsOptions:      ',wms[el].crsOptions)
-	print('\tStyles:          ',wms[el].styles)
+    print('*'*80)
+    print('\tTitle({}):{}'.format(el,wms[el].title))
+    print('\tQueryable:       ',wms[el].queryable)
+    print('\tOpaque:          ',wms[el].opaque)
+    print('\tBoundingBox:     ',wms[el].boundingBox)
+    print('\tBoundingBoxWGS84:',wms[el].boundingBoxWGS84)
+    print('\tcrsOptions:      ',wms[el].crsOptions)
+    print('\tStyles:          ',wms[el].styles)
 
 print('*'*80)
 print('Operations:',[op.name for op in wms.operations])
@@ -30,7 +30,7 @@ print('GetMap formats:',wms.getOperationByName('GetMap').formatOptions)
 #box = 0.01
 """
 
-box = 2000 # m?
+box = 1000 # m?
 x=234314#m?
 y=5067580 #m?
 
@@ -38,73 +38,73 @@ picsize = 512
 #possible (boxsize/picsize) configurations to detect fields: 1000/512, 2000/512 , 1000 /256
 
 img = wms.getmap(
-	layers=['OI.OrthoimageCoverage'],
-	styles=[],
-	srs='EPSG:3857',
-	bbox=(x - box, y - box, x + box, y + box),
-	size=(picsize,picsize), #W,H px
-	format='image/png',
-	transparent=False
+    layers=['OI.OrthoimageCoverage'],
+    styles=[],
+    srs='EPSG:3857',
+    bbox=(x - box, y - box, x + box, y + box),
+    size=(picsize,picsize), #W,H px
+    format='image/png',
+    transparent=False
 )
 with open('example_image_mosaic.png','wb') as out:
-	out.write(img.read())
+    out.write(img.read())
 
-"""
+
 def green_detection():
-	img = cv2.imread('example_image_mosaic.png')
-	# Green fields detection
-	hsv= cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.imread('example_image_mosaic.png')
+    # Green fields detection
+    hsv= cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	#Threshold for detecting green
-	lower = np.array([33, 58, 33])
-	upper = np.array([120,250,120])
-	
-	# Apply mask
-	mask = cv2.inRange(hsv, lower, upper)
-	output = cv2.bitwise_and(img, img, mask = mask)
+    #Threshold for detecting green
+    lower = np.array([33, 58, 33])
+    upper = np.array([120,250,120])
+    
+    # Apply mask
+    mask = cv2.inRange(hsv, lower, upper)
+    output = cv2.bitwise_and(img, img, mask = mask)
 
-	hsv_gray= cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-	green_edges=cv2.Canny(hsv_gray,150,200)
+    hsv_gray= cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    green_edges=cv2.Canny(hsv_gray,150,200)
 
-	cv2.imwrite('green_hsv.png',hsv)
-	cv2.imwrite('green.png',output)
-	cv2.imwrite('green_edges.png', green_edges)
+    cv2.imwrite('green_hsv.png',hsv)
+    cv2.imwrite('green.png',output)
+    cv2.imwrite('green_edges.png', green_edges)
+
 
 def contour_detection():
-	# Get the contours
-	canny_img= cv2.imread('green_edges.png',0)
-	img= cv2.imread('example_image_mosaic.png',1)
-	img2= cv2.imread('example_image_mosaic.png',1)
-	_, contours, hierarchy=cv2.findContours(canny_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	cv2.drawContours(img,contours,-1,(0,255,0))
-	cv2.imwrite('contours.png',img)
+    # Get the contours
+    canny_img= cv2.imread('green_edges.png',0)
+    img= cv2.imread('example_image_mosaic.png',1)
+    img2= cv2.imread('example_image_mosaic.png',1)
+    _, contours, hierarchy=cv2.findContours(canny_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img,contours,-1,(0,255,0))
+    cv2.imwrite('contours.png',img)
 
-	#Filter the contours corresponding to football fields
+    #Filter the contours corresponding to football fields
 
-	MinHeight= 20
-	MaxHeight= 45
-	MinWidth= 20
-	MaxWidth= 45
-	k=0
-	filteredContours=[]
-	fields_x_positions=[]
-	fields_y_positions=[]
-	while(k < len(contours)):
-		bounds= cv2.boundingRect(contours[k])
-		x,y,w,h = bounds
-		if ((h > MinHeight) and (h < MaxHeight) and (w > MinWidth)):
-			filteredContours.append(contours[k])
+    MinHeight= 20
+    MaxHeight= 45
+    MinWidth= 20
+    MaxWidth= 45
+    k=0
+    filteredContours=[]
+    fields_x_positions=[]
+    fields_y_positions=[]
+    while(k < len(contours)):
+        bounds= cv2.boundingRect(contours[k])
+        x,y,w,h = bounds
+        if ((h > MinHeight) and (h < MaxHeight) and (w > MinWidth)):
+            filteredContours.append(contours[k])
 
-			# We save the X and Y pixel position of the fields
-			fields_y_positions.append(y)
-			fields_x_positions.append(x)
-		k+=1
+            # We save the X and Y pixel position of the fields
+            fields_y_positions.append(y)
+            fields_x_positions.append(x)
+        k += 1
 
-	cv2.drawContours(img2,filteredContours,-1,(0,255,0))
-	cv2.imwrite('filtered_contours.png',img2)	
+    cv2.drawContours(img2,filteredContours,-1,(0,255,0))
+    cv2.imwrite('filtered_contours.png',img2)    
 
 
 if __name__ == "__main__":
-	green_detection()
-	contour_detection()
-"""
+    green_detection()
+    contour_detection()
